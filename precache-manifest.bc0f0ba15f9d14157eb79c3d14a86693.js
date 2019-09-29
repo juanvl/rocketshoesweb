8adb2bea806c3223378ccef11c89dc3645ad5daf
Juan <jnvleite@gmail.com>
2019-09-29 19:38:57 -0300

    Updates

---
+++ precache-manifest.bc0f0ba15f9d14157eb79c3d14a86693.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0e4dfa3adf3a1e0fffc3d2694591670",
+    "url": "/rocketshoesweb/index.html"
+  },
+  {
+    "revision": "99679038b488e50f6fe5",
+    "url": "/rocketshoesweb/static/css/2.39017165.chunk.css"
+  },
+  {
+    "revision": "99679038b488e50f6fe5",
+    "url": "/rocketshoesweb/static/js/2.a258b51e.chunk.js"
+  },
+  {
+    "revision": "4b5b2b64eaeefe00af77",
+    "url": "/rocketshoesweb/static/js/main.dbd98b58.chunk.js"
+  },
+  {
+    "revision": "86a4282d79a6cba12ba3",
+    "url": "/rocketshoesweb/static/js/runtime~main.2127db44.js"
+  },
+  {
+    "revision": "3c9a1ce505a48f2c15ba9505b6f5667b",
+    "url": "/rocketshoesweb/static/media/bg.3c9a1ce5.svg"
+  },
+  {
+    "revision": "879557c5bdbbd3743a7680c651e31d08",
+    "url": "/rocketshoesweb/static/media/logo.879557c5.svg"
+  }
+]);
